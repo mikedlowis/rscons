2c7713c9e7d67f65f0d22d49c6c4440bf508f946
Michael D. Lowis <mike.lowis@gentex.com>
2014-07-16 13:44:08 -0400

    Added more default cosntruction varaibles

--- lib/rscons/builders/shared_library.rb
+++ lib/rscons/builders/shared_library.rb
@@ -9,6 +9,11 @@ module Rscons
       # @return [Hash] Default construction variables for the builder.
       def default_variables(env)
         defaults = {
+          'SHCC' => nil,
+          'SHCCCMD' => ['${SHCC}', '-c', '-o', '${_TARGET}', '${CCDEPGEN}', '${INCPREFIX}${CPPPATH}', '${CPPFLAGS}', '${SHCFLAGS}', '${SHCCFLAGS}', '${_SOURCES}'],
+          'SHCFLAGS' => [],
+          'SHCCFLAGS' => [],
+
           'SHLD' => nil,
           'SHLDCMD' => ['${SHLD}', '-o', '${_TARGET}', '${SHLDFLAGS}', '${_SOURCES}', '${SHLIBDIRPREFIX}${LIBPATH}', '${SHLIBLINKPREFIX}${LIBS}'],
           'SHLDFLAGS' => [],
@@ -19,15 +24,21 @@ module Rscons
         #    gcc -o source/libsof/libsof.os -c -Wall -Werror -std=c99 -fPIC source/libsof/libsof.c
         #    gcc -o build/lib/libsof.dylib -dynamiclib source/libsof/libsof.os -Lbuild/lib
         # Cygwin:
+        #    gcc -o source/libsof/libsof.os -c -Wall -Werror -std=c99 source/libsof/libsof.c
+        #    gcc -o build/lib/cygsof.dll -Wl,-no-undefined -shared -Wl,--out-implib=build/lib/libsof.dll.a -Wl,--export-all-symbols -Wl,--enable-auto-import -Wl,--whole-archive source/libsof/libsof.os -Wl,--no-whole-archive -Lbuild/lib
+        # Mingw:
         #
         case Object.const_get("RUBY_PLATFORM")
         when /mingw|cygwin/
           defaults['SHLIBSUFFIX'] = '.dll'
         when /darwin/
           defaults['SHLIBSUFFIX'] = '.dylib'
+          defaults['SHCCFLAGS'] += ['-fPIC']
           defaults['SHLDFLAGS'] += ['-dynamiclib']
         else
           defaults['SHLIBSUFFIX'] = '.so'
+          defaults['SHCCFLAGS'] += ['-fPIC']
+          defaults['SHLDFLAGS'] += ['-shared']
         end
         defaults
       end
@@ -43,16 +54,16 @@ module Rscons
       # @return [String,false]
       #   Name of the target file on success or false on failure.
       def run(target, sources, cache, env, vars)
-        # build sources to linkable objects
-        objects = env.build_sources(sources, env.expand_varref(["${OBJSUFFIX}", "${LIBSUFFIX}"], vars).flatten, cache, vars)
-        if objects
-          vars = vars.merge({
-            '_TARGET' => target,
-            '_SOURCES' => objects,
-          })
-          command = env.build_command("${ARCMD}", vars)
-          standard_build("AR #{target}", target, command, objects, env, cache)
-        end
+        ## build sources to linkable objects
+        #objects = env.build_sources(sources, env.expand_varref(["${OBJSUFFIX}", "${LIBSUFFIX}"], vars).flatten, cache, vars)
+        #if objects
+        #  vars = vars.merge({
+        #    '_TARGET' => target,
+        #    '_SOURCES' => objects,
+        #  })
+        #  command = env.build_command("${ARCMD}", vars)
+        #  standard_build("AR #{target}", target, command, objects, env, cache)
+        #end
       end
     end
   end
